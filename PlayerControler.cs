79fe0ab9aa7e12276a195c8738836218830ec8b9
pawciomaster <49043641+pawciomaster@users.noreply.github.com>
2020-01-20 19:15:38 +0100

    Add files via upload

---
+++ PlayerControler.cs
@@ -0,0 +1,87 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerControler : MonoBehaviour
+{
+    public static PlayerControler instance;
+    public float MoveSpeed;
+    private Vector2 moveInput;
+    public Rigidbody2D theRB;
+    public Transform gunArm;
+    private Camera theCam;
+    public Animator anim;
+    public GameObject bulletToFire;
+    public Transform firePoint;
+    public float timeBetweenShots;
+    private float shotCounter;
+
+    private void Awake()
+    {
+        instance = this;
+    }
+    // Start is called before the first frame update
+    void Start()
+    {
+        theCam = Camera.main;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        moveInput.x = Input.GetAxisRaw("Horizontal");
+        moveInput.y = Input.GetAxisRaw("Vertical");
+        moveInput.Normalize();
+        theRB.velocity = moveInput * MoveSpeed;
+
+        Vector3 mousePos = Input.mousePosition;
+        Vector3 screenPoint =theCam.WorldToScreenPoint(transform.localPosition);
+
+        if(mousePos.x < screenPoint.x)
+        {
+            transform.localScale = new Vector3(-1f, 1f, 1f);
+            gunArm.localScale = new Vector3(-1f, -1f, 1f);
+        }
+        else
+        {
+            transform.localScale = Vector3.one;
+            gunArm.localScale = Vector3.one;
+        }
+
+        //rotate pistol
+        Vector2 offset = new Vector2(mousePos.x - screenPoint.x, mousePos.y - screenPoint.y);
+        float angle = Mathf.Atan2(offset.y, offset.x) * Mathf.Rad2Deg;
+        gunArm.rotation = Quaternion.Euler(0, 0, angle);
+
+
+
+        if(Input.GetMouseButtonDown(0))
+        {
+            Instantiate(bulletToFire, firePoint.position, firePoint.rotation);
+            shotCounter = timeBetweenShots;
+        }
+        if(Input.GetMouseButton(0))
+        {
+            shotCounter -= Time.deltaTime;
+            if(shotCounter <= 0)
+            {
+                Instantiate(bulletToFire, firePoint.position, firePoint.rotation);
+                shotCounter = timeBetweenShots;
+            }
+        }
+
+
+
+
+        if(moveInput != Vector2.zero)
+        {
+            anim.SetBool("isMoving", true);
+        }else
+        {
+            anim.SetBool("isMoving", false);
+        }
+        
+
+
+    }
+}
---
+++ UIController.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class UIController : MonoBehaviour
+{
+    public static UIController instance;
+    public Slider healthSlider;
+    public Text healthText;
+
+    public void Awake()
+    {
+        instance = this;
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
